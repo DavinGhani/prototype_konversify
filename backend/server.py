f111a22d60b3d1e390912494b9d493e7a312a161
Davin <ghaniananta@gmail.com>
2025-11-20 18:51:16 +0700

    upload file ke repo

---
+++ backend/server.py
@@ -0,0 +1,28 @@
+import pandas as pd
+from flask import Flask, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+CSV_FILE = 'Data_Final.csv' 
+
+try:
+    # Membaca CSV
+    data_df = pd.read_csv(CSV_FILE)
+    print(f"✅ File '{CSV_FILE}' berhasil dimuat.")
+except FileNotFoundError:
+    print(f"❌ ERROR: File '{CSV_FILE}' tidak ditemukan!")
+    data_df = pd.DataFrame()
+
+@app.route('/api/customers', methods=['GET'])
+def get_customers():
+    if data_df.empty:
+        return jsonify([]), 200 
+    
+    df_clean = data_df.fillna('')
+    customers_json = df_clean.to_dict(orient='records')
+    return jsonify(customers_json)
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
